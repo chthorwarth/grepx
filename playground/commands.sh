eb73df1006bb65d2cc4928df483736f7a0bdb329
Christian Thorwarth <christian.thorwarth@study.thws.de>
2025-12-23 13:11:20 +0100

    Added a little playground to play around with the original grep

---
+++ playground/commands.sh
@@ -0,0 +1,12 @@
+grep "Data Science" folder_stage1/datascience.txt
+grep "Data Science" folder_stage1/datascience.txt folder_stage1/datascience2.txt
+grep "Data Science" folder_stage1/*.txt
+grep -r "Data Science" folder_stage1
+grep -n "Data Science" folder_stage1/datascience.txt
+grep -r -c "Data Science" folder_stage1
+grep -v "Data Science" folder_stage1/datascience.txt
+grep -i "data science" folder_stage1/datascience.txt
+grep -q "Data Science" folder_stage1/datascience.txt
+grep -C 2 "Data Science" folder_stage1/datascience.txt
+grep -f pattern.txt folder_stage1/datascience.txt
+grep -e "Data Science" -e "Cyber Security" folder_stage1/datascience.txt
