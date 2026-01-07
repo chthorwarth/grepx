24f7e04626fc0d1ff2ffead5035b5a05d753d5d9
Christian Thorwarth <christian.thorwarth@study.thws.de>
2026-01-07 11:42:07 +0100

    Improved search.c + test cases

--- src/main.c
+++ src/main.c
@@ -16,12 +16,13 @@ int main(int argc, char *argv[]) {
     char *context_endptr = NULL;
 
     parse(&argc, argv, &opts, context_endptr);
-
     validate(&opts, context_endptr);
 
     for (int i = 0; i < opts.path_count; i++) {
         searchInFile(opts.paths[i], &opts);
     }
+    if (opts.path_count == 0)
+        searchStream(stdin, NULL, &opts);
 
     return 0;
 }
--- src/search.h
+++ src/search.h
@@ -1,8 +1,9 @@
-#ifndef SEARCH_H
-#define SEARCH_H
+#ifndef SEARCH_CORE_H
+#define SEARCH_CORE_H
 
 #include "grep_options.h"
 
 int searchInFile(const char *filename, grep_options_t *opts);
+int searchStream(FILE *stream, const char *filename, grep_options_t *opts);
 
 #endif
